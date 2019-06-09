c80fbfec47f2c8f28373d7099f432ec9f3bc9194
closedCaptions <37214242+closedCaptions@users.noreply.github.com>
2019-06-08 23:54:02 -1100

    Refactor commit!!
    - Fix bug in winning methods
    - Add a game loop via thread
    - Add interactibility
    
    - Refactor Display()
    - Refactor Minimax()

--- tictactoe-console/Game.cs
+++ tictactoe-console/Game.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 using System.Numerics;
 
 namespace tictactoe {
-    public class Game {
+
+    class Game {
 
         #region Variable
 
         #region 2D Array - `board`
+
         private readonly State[,] board;
+
         public State this[int y, int x] { // Declare indexer
             get {
                 return board[y, x];
@@ -23,7 +25,16 @@ namespace tictactoe {
 
         public State this[Vector2 index] { // Declare indexer
             get {
-                return this[(int)index.Y, (int)index.X];
+                return this[(int) index.Y, (int) index.X];
+            }
+            set {
+                this[(int) index.Y, (int) index.X] = value;
+            }
+        }
+
+        public State this[Vector3 index] {
+            get {
+                return this[(int) index.Y, (int) index.X];
             }
             set {
                 this[(int) index.Y, (int) index.X] = value;
@@ -33,7 +44,8 @@ namespace tictactoe {
         public int GetLength (int dimension) {
             return board.GetLength(dimension);
         }
-        #endregion
+
+        #endregion 2D Array - `board`
 
         private int maxDepth;
         private Vector2 latestMove;
@@ -43,9 +55,10 @@ namespace tictactoe {
                 return GetAvailSlots().Count == 0 || Winning(State.O) || Winning(State.X);
             }
         }
+
         public int nLength;
 
-        #endregion
+        #endregion Variable
 
         #region Constructor
 
@@ -57,50 +70,60 @@ namespace tictactoe {
             this.maxDepth = maxDepth;
         }
 
-        #endregion
+        #endregion Constructor
 
         #region Method
 
         public void Display () {
-            for (int y = 0; y < this.GetLength(0); y++) {
+            for (int y = 0; y < this.nLength; y++) {
                 if (y != 0)
-                    Console.Write("--- --- ---\n");
-                for (int x = 0; x < this.GetLength(1); x++) {
-                    string toWrite = " ";
+                    Console.WriteLine(string.Join("--- ", new string[this.nLength + 1]));
+
+                for (int x = 0; x < this.nLength; x++) {
+                    string toWrite = "  ";
                     switch (this[y, x]) {
                         case State.O:
-                            toWrite = "O";
+                            toWrite = "O ";
                             break;
+
                         case State.X:
-                            toWrite = "X";
+                            toWrite = "X ";
                             break;
                     }
-                    Console.Write(" {0} ", toWrite);
-                    if (x != this.GetLength(1) - 1)
-                        Console.Write(" ");
+                    toWrite += (x != this.nLength - 1 ? "|" : "");
+                    
+                    Console.Write(" {0}", toWrite);
                 }
-                Console.Write("\n");
+                Console.WriteLine();
             }
         }
 
         #region Minimax
 
-        public Vector3 Minimax (bool maximizing) {
-            return Minimax(this, maxDepth, maximizing, GetAvailSlots());
+        public Vector2 Minimax (bool maximizing) {
+            List<Vector2> availSlots = GetAvailSlots();
+            if (availSlots.Count == this.nLength * this.nLength) {  // He is first
+                Random rnd = new Random();
+                return new Vector2(rnd.Next(0, this.nLength), rnd.Next(0, this.nLength));
+            } else {
+                Vector3 bestChoice = Minimax(this, this.maxDepth, maximizing, availSlots);
+                return new Vector2(bestChoice.X, bestChoice.Y);
+            };
         }
 
         private Vector3 Minimax (Game game, int depth, bool maximizing, List<Vector2> availSlots) {
             /**
-             * x = x index
-             * y = y index
-             * z = score
+             * NOTE:
+             * x is x index
+             * y is y index
+             * z is score
              */
 
             if (game.GetScore(depth) != 0 || availSlots.Count == 0)
                 return new Vector3(-1, -1, GetScore(depth));
 
             List<Vector3> moveNodes = new List<Vector3>();
-            // Find pausible moves
+            // Find possible moves
             foreach (Vector2 slot in availSlots) {
                 Vector2[] tmp = new Vector2[availSlots.Count];
                 availSlots.CopyTo(tmp);
@@ -108,16 +131,15 @@ namespace tictactoe {
                 copy.Remove(slot);
                 game[slot] = (maximizing ? State.O : State.X);
 
-                moveNodes.Add(new Vector3(slot, Minimax (game, depth - 1, !maximizing, copy).Z));
+                moveNodes.Add(new Vector3(slot, Minimax(game, depth - 1, !maximizing, copy).Z));
 
-                game[slot] = State.Blank; // reset back to blank
-                game.latestMove = this.latestMove;
+                game[slot] = State.Blank; // Reset back to blank
             }
 
             // Actual minimax
-            if (maximizing) { // Bot is playing
+            if (maximizing) { // O is playing
                 return moveNodes.AsEnumerable().Aggregate((acc, val) => (val.Z > acc.Z ? val : acc));
-            } else { // Human is playing
+            } else { // X is playing
                 return moveNodes.AsEnumerable().Aggregate((acc, val) => (val.Z < acc.Z ? val : acc));
             }
         }
@@ -131,47 +153,37 @@ namespace tictactoe {
         }
 
         private bool WinCol (State state) {
-            int winSum = 0;
             for (int i = 0; i < this.nLength; i++) {
-                if (winSum == this.nLength)
-                    return true;
+                if (this[i, (int) latestMove.X] != state)
+                    break; // Will stop the i++ operation from happening
 
-                if (this[i, (int)latestMove.X] != state) {
-                    winSum = 0;
-                }else {
-                    winSum++;
-                }
+                if (i == this.nLength - 1)
+                    return true;
             }
 
             return false;
         }
 
         private bool WinRow (State state) {
-            int winSum = 0;
             for (int i = 0; i < this.nLength; i++) {
-                if (winSum == this.nLength)
-                    return true;
+                if (this[(int) latestMove.Y, i] != state)
+                    break; // Will stop the i++ operation from happening
 
-                if (this[(int)latestMove.Y, i] != state) {
-                    winSum = 0;
-                } else {
-                    winSum++;
-                }
+                if (i == this.nLength - 1)
+                    return true;
             }
 
             return false;
         }
 
         private bool WinDiag (State state) {
-            int winSum = 0;
-            for (int i = 0; i < this.nLength; i++) {
-                if (winSum == this.nLength)
-                    return true;
+            if (latestMove.X == latestMove.Y) {
+                for (int i = 0; i < nLength; i++) {
+                    if (this[i, i] != state)
+                        break; // Will stop the i++ operation from happening
 
-                if (this[i, i] != state) {
-                    winSum = 0;
-                } else {
-                    winSum++;
+                    if (i == nLength - 1)
+                        return true;
                 }
             }
 
@@ -179,37 +191,32 @@ namespace tictactoe {
         }
 
         private bool WinAntiDiag (State state) {
-            int winSum = 0;
-            for (int i = 0; i < this.nLength; i++) {
-                if (winSum == this.nLength)
-                    return true;
+            for (int i = 0; i < nLength; i++) {
+                if (this[nLength - 1 - i, i] != state)
+                    break; // Will stop the i++ operation from happening
 
-                if (this[i, (this.nLength - 1) - i] != state) {
-                    winSum = 0;
-                } else {
-                    winSum++;
-                }
+                if (i == nLength - 1)
+                    return true;
             }
 
             return false;
         }
 
-        #endregion
+        #endregion Winning method
 
         private int GetScore (int depth) {
             int toSubtract = (maxDepth - depth);
 
             if (Winning(State.O)) { // Bot wins
                 return int.MaxValue - toSubtract;
-            }else if (Winning(State.X)) { // Human wins
+            } else if (Winning(State.X)) { // Human wins
                 return int.MinValue + toSubtract;
-            }else { // Round draw (No available slots) or depth is zero
+            } else { // Round draw (No available slots) or depth is zero
                 return 0;
             }
-
         }
 
-        private List<Vector2> GetAvailSlots () {
+        public List<Vector2> GetAvailSlots () {
             List<Vector2> dest = new List<Vector2>();
 
             for (int y = 0; y < this.GetLength(0); y++) {
@@ -222,24 +229,32 @@ namespace tictactoe {
             return dest;
         }
 
-        #endregion
+        #endregion Helper method
 
-        #endregion
+        #endregion Minimax
 
-        #endregion
+        #endregion Method
 
     }
 
     static class Utility {
-
-        public delegate bool ReturnBool <T> (T value);
-
         public static void Populate<T> (this T[,] array, T populateValue) {
             for (int y = 0; y < array.GetLength(0); ++y) {
                 for (int x = 0; x < array.GetLength(1); ++x) {
                     array[y, x] = populateValue;
                 }
             }
+        }
+
+        public static int AskInput (this string msg, string failMsg) { // Uses Console.Write()
+            Console.Write(msg);
+            int result;
+            bool successful = int.TryParse(Console.ReadLine(), out result);
+
+            if (!successful)
+                return AskInput(failMsg, failMsg);
+
+            return result;
         }
     }
 }
--- tictactoe-console/Program.cs
+++ tictactoe-console/Program.cs
@@ -6,28 +6,52 @@ using System.Numerics;
 using System.Threading;
 
 public enum State { Blank, X, O }
+/**
+ * NOTE:
+ * X is minimizer
+ * O is maximizer
+ */
 
 namespace tictactoe {
-    public class Program {
+    class Program {
 
-        public static Game game = new Game(3, 15);
+        private static Game game = new Game(3, int.MaxValue);
 
         private static void Main (string[] args) {
-            Thread gameLoop = new Thread(new ThreadStart(MyMethod));
+            Thread gameLoop = new Thread(new ThreadStart(ThreadUpdate));
+            game.Display();
             gameLoop.Start();
+            
         }
 
-        private static void MyMethod () {
+        private static void ThreadUpdate () {
             while (true) {
+                int X = Utility.AskInput("\nInput X value:", "Try again, integer only\n") - 1;
+                int Y = Utility.AskInput("\nInput Y value:", "Try again, integer only\n") - 1;
+                if (game[Y, X] != State.Blank) {
+                    
+                }
+                game[Y, X] = State.X;
+                Console.Clear();
                 game.Display();
-                var 
-                game[game.Minimax(false).X ] = State.O;
-                [game.Minimax(true)];
-                
-                Thread.Sleep(1000 * 5); // 2 seconds
 
-                if (game.Finished)
+                if (game.Finished) // Check if finished to avoid out of bounds
                     break;
+
+                game[game.Minimax(true)] = State.O;
+                Console.Clear();
+                game.Display();
+
+                if (game.Finished) // Check if finished to avoid out of bounds
+                    break;
+            }
+
+            if (game.Winning(State.X)) { // Report win for X
+                Console.WriteLine("X wins!");
+            } else if (game.Winning(State.O)) { // Report win for O
+                Console.WriteLine("O wins!");
+            } else { // Report draw
+                Console.WriteLine("Draw!");
             }
         }
     }
