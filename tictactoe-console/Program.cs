75fd810d7fc0427d8e282bbdce7ebf6c0777401b
closedCaptions <37214242+closedCaptions@users.noreply.github.com>
2019-06-08 23:56:52 -1100

    Edit comments and if-else structure

--- tictactoe-console/Program.cs
+++ tictactoe-console/Program.cs
@@ -21,7 +21,6 @@ namespace tictactoe {
             Thread gameLoop = new Thread(new ThreadStart(ThreadUpdate));
             game.Display();
             gameLoop.Start();
-            
         }
 
         private static void ThreadUpdate () {
@@ -29,7 +28,7 @@ namespace tictactoe {
                 int X = Utility.AskInput("\nInput X value:", "Try again, integer only\n") - 1;
                 int Y = Utility.AskInput("\nInput Y value:", "Try again, integer only\n") - 1;
                 if (game[Y, X] != State.Blank) {
-                    
+                    // TODO: Add stuff here that will ask again if player is out of bounds
                 }
                 game[Y, X] = State.X;
                 Console.Clear();
@@ -46,13 +45,12 @@ namespace tictactoe {
                     break;
             }
 
-            if (game.Winning(State.X)) { // Report win for X
+            if (game.Winning(State.X)) // Report win for X
                 Console.WriteLine("X wins!");
-            } else if (game.Winning(State.O)) { // Report win for O
+            else if (game.Winning(State.O)) // Report win for O
                 Console.WriteLine("O wins!");
-            } else { // Report draw
+            else // Report draw
                 Console.WriteLine("Draw!");
-            }
         }
     }
 }
